68271409f60373a328b9fbe8a9203686d3fe5ffe
LollosoSi <lollosositv@gmail.com>
2024-02-04 17:46:41 +0100

    Selective repeat packetizer (part 1)

---
+++ packetization/selectiverepeat/SelectiveRepeatPacketizer.cpp
@@ -0,0 +1,74 @@
+/*
+ * SelectiveRepeatPacketizer.cpp
+ *
+ *  Created on: 4 Feb 2024
+ *      Author: Andrea Roccaccino
+ */
+
+#include "SelectiveRepeatPacketizer.h"
+
+SelectiveRepeatPacketizer::SelectiveRepeatPacketizer() {
+	// TODO Auto-generated constructor stub
+
+}
+
+SelectiveRepeatPacketizer::~SelectiveRepeatPacketizer() {
+	// TODO Auto-generated destructor stub
+}
+
+bool SelectiveRepeatPacketizer::next_packet_ready() {
+	return (Settings::control_packets || !frames.empty());
+}
+
+RadioPacket& SelectiveRepeatPacketizer::next_packet() {
+	if (Settings::control_packets) {
+		if (frames.empty()) {
+			return (get_empty_packet());
+		} else {
+			if (current_packet_counter == frames.front().packets.size()) {
+				if (resend_list.empty())
+					return (frames.front().packets[current_packet_counter]);
+				else {
+					int rs = resend_list.front();
+					resend_list.pop_front();
+					return (frames.front().packets[rs]);
+				}
+			} else {
+				return (frames.front().packets[current_packet_counter++]);
+			}
+		}
+	} else {
+		if (frames.empty()) {
+			// Undefined behaviour
+			std::cout << "Requested frame when no one is available\n";
+			exit(2);
+		} else {
+			if (current_packet_counter == frames.front().packets.size()) {
+				if (resend_list.empty())
+					return (frames.front().packets[current_packet_counter]);
+				else {
+					int rs = resend_list.front();
+					resend_list.pop_front();
+					return (frames.front().packets[rs]);
+				}
+			} else {
+				return (frames.front().packets[current_packet_counter++]);
+			}
+		}
+	}
+
+}
+
+void SelectiveRepeatPacketizer::free_frame(Frame<RadioPacket> &frame) {
+
+	while (!frame.packets.empty()) {
+		delete[] frame.packets.front().data;
+		frame.packets.pop_front();
+	}
+
+}
+
+RadioPacket& SelectiveRepeatPacketizer::get_empty_packet() {
+	static RadioPacket rp = { 0, new uint8_t[1] { 0 } };
+	return (rp);
+}
---
+++ packetization/selectiverepeat/SelectiveRepeatPacketizer.h
@@ -0,0 +1,37 @@
+/*
+ * SelectiveRepeatPacketizer.h
+ *
+ *  Created on: 4 Feb 2024
+ *      Author: Andrea Roccaccino
+ */
+
+#pragma once
+
+#include "../../interfaces/PacketHandler.h"
+#include "../../structures/RadioPacket.h"
+
+#include "../../settings/Settings.h"
+
+class SelectiveRepeatPacketizer: public PacketHandler<RadioPacket> {
+public:
+	SelectiveRepeatPacketizer();
+	virtual ~SelectiveRepeatPacketizer();
+
+	bool next_packet_ready();
+	RadioPacket& next_packet();
+	RadioPacket& get_empty_packet();
+
+protected:
+	unsigned int current_packet_counter = 0;
+	std::deque<int> resend_list;
+	void received_ok() {
+		current_packet_counter = 0;
+		free_frame(frames.front());
+		frames.pop_front();
+		resend_list.clear();
+	}
+	void free_frame(Frame<RadioPacket> &frame);
+
+
+};
+
