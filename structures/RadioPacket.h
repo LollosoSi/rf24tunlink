04fda89fb5c1957069414b994c8c42ba366f93e6
LollosoSi <lollosositv@gmail.com>
2024-02-04 11:24:11 +0100

    RadioPacket structure

---
+++ structures/RadioPacket.h
@@ -0,0 +1,15 @@
+/*
+ * RadioPacket.h
+ *
+ *  Created on: 4 Feb 2024
+ *      Author: andrea
+ */
+
+#pragma once
+
+#include <RF24.h>
+
+struct RadioPacket{
+		uint8_t info = 0;
+		uint8_t *data = 0;
+};
