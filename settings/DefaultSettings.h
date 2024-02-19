59945f7e4c756b50bcdfeeadfe1680ac0472970a
LollosoSi <lollosositv@gmail.com>
2024-02-19 19:03:07 +0100

    Double radio first settings

--- settings/DefaultSettings.h
+++ settings/DefaultSettings.h
@@ -27,7 +27,7 @@ uint16_t max_radio_silence = 600;
 
 bool variable_rate = true;
 
-int ce_pin = 25;
+int ce_pin = 24;
 int csn_pin = 0;
 //unsigned long spi_speed = 6000000; //(10000000-4000000);
 //unsigned long spi_speed = 5300000; //(10000000-4000000);
@@ -53,3 +53,45 @@ uint8_t *radio_retries_tuned = new uint8_t[3]{ 15, 15, 15 };
 
 
 }
+
+namespace Settings::DUAL_RF24 {
+
+uint16_t max_radio_silence = 600;
+
+bool variable_rate = true;
+
+int ce_0_pin = 24;
+int csn_0_pin = 0;
+
+int ce_1_pin = 25;
+int csn_1_pin = 1;
+//unsigned long spi_speed = 6000000; //(10000000-4000000);
+//unsigned long spi_speed = 5300000; //(10000000-4000000);
+unsigned long spi_speed = 5200000; //(10000000-4000000);
+
+
+rf24_datarate_e data_rate = RF24_2MBPS;
+rf24_pa_dbm_e radio_power = RF24_PA_MAX;
+rf24_crclength_e crc_length = RF24_CRC_8;
+
+uint8_t radio_delay = 1;
+uint8_t radio_retries = 15;
+
+uint8_t channel_0 = 2;
+uint8_t channel_1 = 124;
+uint8_t address_bytes = 3;
+uint8_t *address_0_1 = new uint8_t[3] { '4', 'N', 'o' };
+uint8_t *address_0_2 = new uint8_t[3] { '5', 'N', 'o' };
+uint8_t *address_0_3 = new uint8_t[3] { '6', 'N', 'o' };
+
+uint8_t *address_1_1 = new uint8_t[3] { '7', 'N', 'o' };
+uint8_t *address_1_2 = new uint8_t[3] { '8', 'N', 'o' };
+uint8_t *address_1_3 = new uint8_t[3] { '9', 'N', 'o' };
+
+uint8_t *radio_delay_tuned = new uint8_t[3]{ 6, 2, 1 };
+uint8_t *radio_retries_tuned = new uint8_t[3]{ 15, 15, 15 };
+
+
+
+}
+
