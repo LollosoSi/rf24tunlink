707cde2fdfa6b95c125f1d527a53632ef9bad36c
LollosoSi <lollosositv@gmail.com>
2024-06-05 12:12:29 +0200

    Specified install service for primary and secondary

--- installservice.sh
+++ installservice.sh
@@ -1,9 +1,29 @@
+if [ -z "$1" ]
+  then
+    echo "No argument supplied, you must specify whether to install as \"primary\" or \"secondary\""
+    exit;
+fi
+
+if [ "$1" == "primary" ]
+then
 printf "
 ARG1=${PWD}/presets/radio_config.txt
 ARG2=${PWD}/presets/tunlink_config.txt
 ARG3=${PWD}/presets/harq_config.txt
 ARG4=${PWD}/presets/primary
 " > .progconf
+elif [ "$1" == "secondary" ]
+then
+printf "
+ARG1=${PWD}/presets/radio_config.txt
+ARG2=${PWD}/presets/tunlink_config.txt
+ARG3=${PWD}/presets/harq_config.txt
+ARG4=${PWD}/presets/secondary
+" > .progconf
+else
+ echo "Bad argument supplied, you must specify whether to install as \"primary\" or \"secondary\""
+ exit;
+fi
 
 printf "[Unit]
 Description=rf24tunlink2
