b9cbd84f8dea687fccc0682a027fd2d6d134079f
LollosoSi <lollosositv@gmail.com>
2024-06-13 11:29:21 +0200

    More changes for configs

--- ecc_config.sh
+++ ecc_config.sh
@@ -1,6 +1,8 @@
 if [ -z "$1" ]
   then
     echo "No argument supplied, you must specify the ECC bytes"
+    echo "Current config: "
+    echo "$(<presets/rs_config )"
     exit;
 fi
 
@@ -13,18 +15,15 @@ fi
 
 val=$(expr $pl_sz - $1)
 
-printf "
-payload_size=$pl_sz
-
-#	The data bytes for the Reed-Solomon codec
+printf "#	The data bytes for the Reed-Solomon codec
 #	Type:	uint8
 data_bytes=$val
 
 #	The ECC bytes for the Reed-Solomon codec (note, one byte might be used as CRC, this element must be >0)
 #	Type:	uint8
 ecc_bytes=$1
+
+payload_size=$pl_sz
 " > presets/rs_config
 
-./stopservice.sh
-./startservice.sh
-./restartservice.sh
+./stopservice.sh && ./startservice.sh
--- power_bitrate_config.sh
+++ power_bitrate_config.sh
@@ -1,18 +1,21 @@
 if [ -z "$1" ]
   then
     echo "You must specify the power level as the first argument (Accepted values: 0 - MIN, 1 - LOW, 2 - HIGH, 3 - MAX)"
+    echo "Current config: "
+    echo "$(<presets/radio_pb_config )"
     exit;
 fi
 
 if [ -z "$2" ]
   then
     echo "You must specify the bitrate as the second argument (Accepted values: 0 - 1Mbps, 1 - 2Mbps, 2 - 250kbps )"
+    echo "Current config: "
+    echo "$(<presets/radio_pb_config )"
     exit;
 fi
 
 
-printf "
-#	The modulation speed of the radios (Accepted values: 0 - 1Mbps, 1 - 2Mbps, 2 - 250kbps )
+printf "#	The modulation speed of the radios (Accepted values: 0 - 1Mbps, 1 - 2Mbps, 2 - 250kbps )
 #	Type:	uint8
 data_rate=$2
 
@@ -21,6 +24,4 @@ data_rate=$2
 radio_power=$1
 " > presets/radio_pb_config
 
-./stopservice.sh
-./startservice.sh
-./restartservice.sh
+./stopservice.sh && ./startservice.sh
