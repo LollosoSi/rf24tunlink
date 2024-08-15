2626ba2da9bf58b16d4277953a27bbbff6c03269
Andrea <lollosositv@gmail.com>
2024-08-15 19:38:20 +0200

    Add files via upload

--- install_script.sh
+++ install_script.sh
@@ -1,32 +1,38 @@
 #!/bin/bash
 # Set config.txt
+#!/bin/bash
+
 read -p "Do you intend to use the dual rf24 setup? (y/n) " yn
 case $yn in
-    [Yy]* )
+    [Yy]*) 
         sudo grep -qxF 'dtoverlay=spi1-3cs' /boot/firmware/config.txt || 
         sudo printf '[all]\ndtoverlay=spi1-3cs\n' >> /boot/firmware/config.txt
         ;;
-    [Nn]* )
+    [Nn]*) 
         break
         ;;
-    * )
+    *) 
         echo "Please answer yes or no."
         ;;
 esac
 
+#!/bin/bash
+
 read -p "Do you intend to use the action button? (y/n) " yn
 case $yn in
-    [Yy]* )
+    [Yy]*) 
         sudo grep -qxF 'dtoverlay=gpio-key,gpio=4,active_low=1,gpio_pull=up,keycode=30' /boot/firmware/config.txt || 
         sudo printf '[all]\ndtoverlay=gpio-key,gpio=4,active_low=1,gpio_pull=up,keycode=30\n' >> /boot/firmware/config.txt
         ;;
-    [Nn]* )
+    [Nn]*) 
         break
         ;;
-    * )
+    *) 
         echo "Please answer yes or no."
         ;;
 esac
+
+
 
 # Install prerequisites
 cd ~
